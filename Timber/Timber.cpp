2e36f72a2c67e1819d5b1b1967a8c14fbd7f0abe
lucas cazeto <lucas.cazeto@gmail.com>
2019-12-26 18:59:22 +0100

    added more textures and sprites

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -15,15 +15,63 @@ int main()
             window.close();
         }
 
+        // TEXTURES
         Texture textureBackground;
         textureBackground.loadFromFile("graphics/background.png");
+        
+        Texture textureTree;
+        textureTree.loadFromFile("graphics/tree.png");
 
+        Texture textureBee;
+        textureBee.loadFromFile("graphics/bee.png");
+
+        Texture textureCloud;
+        textureCloud.loadFromFile("graphics/cloud.png");
+
+        // SPRITES
         Sprite spriteBackground;
         spriteBackground.setTexture(textureBackground);
         spriteBackground.setPosition(0, 0);
 
+        Sprite spriteTree;
+        spriteTree.setTexture(textureTree);
+        spriteTree.setPosition(810, 0);
+
+        Sprite spriteCloud1;
+        Sprite spriteCloud2;
+        Sprite spriteCloud3;
+        spriteCloud1.setTexture(textureCloud);
+        spriteCloud2.setTexture(textureCloud);
+        spriteCloud3.setTexture(textureCloud);
+        spriteCloud1.setPosition(0, 0);
+        spriteCloud2.setPosition(0, 250);
+        spriteCloud3.setPosition(0, 500);
+
+        Sprite spriteBee;
+        spriteBee.setTexture(textureBee);
+        spriteBee.setPosition(0, 800);
+
+        // SPRITES LOGIC
+        bool beeActive = false;
+        float beeSpeed = 0.0f;
+
+        bool cloud1Active = false;
+        bool cloud2Active = false;
+        bool cloud3Active = false;
+        float cloud1Speed = 0.0f;
+        float cloud2Speed = 0.0f;
+        float cloud3Speed = 0.0f;
+
         window.clear();
+
+        // DRAW
         window.draw(spriteBackground);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
         window.display();
     }
 
